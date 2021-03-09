3a29dfb9968a082328b3b00835ba8066ae1fc1c9
Yash Bahuguna <yashbahuguna10@gmail.com>
2021-03-09 23:42:02 +0530

    first commit

---
+++ src/component/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function Form({ inputText, setInputText, todos, setTodos, setStatus }) {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodohandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText('');
+  };
+
+  const statusHandler = (e)=>{
+    setStatus(e.target.value);
+}
+
+  return (
+    <div>
+      <form>
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button
+          onClick={submitTodohandler}
+          className="todo-button"
+          type="submit"
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+}
